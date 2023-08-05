56c4b54f527e3cb4b11ca2d58da8a9e75a3ccd7e
Patrick <phalloran89@gmail.com>
2023-08-05 19:35:06 +0100

    Add prompt on page load to ask for grid size, add reset button functionality to ask for grid size, clear current sketch and create new grid

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let numOfBoxes = 100;
+document.addEventListener('DOMContentLoaded', () => {
+    numOfBoxes = 
+    prompt("Welcome to Etch-A-Sketch. How big would you like your grid?");
+    createSketch(numOfBoxes);
+})
 
 const titleContainer = document.createElement('div');
 titleContainer.setAttribute('id', 'titleContainer');
@@ -11,33 +15,70 @@ mainTitle.setAttribute('id','mainTitle');
 mainTitle.textContent = "Etch-A-Sketch!";
 titleContainer.appendChild(mainTitle);
 
-function createMultipleBoxes (numOfBoxes) {
+const resetButton = document.createElement('button');
+resetButton.setAttribute('id','resetButton');
+resetButton.textContent = "Reset";
+titleContainer.appendChild(resetButton)
 
-    function createBox (numOfBoxes) {
-        const box = document.createElement('div');
-        box.className = "box";
-        box.setAttribute('id','box' + (i + 1));
-            return box;
-    }
+resetButton.addEventListener('click', resetPrompt);
 
-    const box = document.getElementById('#container');
-        myBoxes = [],
-        i = 0;
-        for (i; i < numOfBoxes; i++) {
-            myBoxes.push(createBox(numOfBoxes));
-            container.appendChild(myBoxes[i]);
-        }
-}
-
-createMultipleBoxes(numOfBoxes);
-
-function boxEventListener(numOfBoxes) {
-    for (let i = 1; i <= numOfBoxes; i++) {
-        let box = document.getElementById('box' + i);
-        box.addEventListener('mousemove', () => {
-            box.style.backgroundColor = `black`;
-        })
-    }
+function resetPrompt() {
+    clearSketch();
+    numOfBoxes = 
+    prompt("How big would you like your grid? Please enter a number.");
+    createSketch(numOfBoxes);
 };
 
-boxEventListener(numOfBoxes);
+function clearSketch() {
+    document.querySelectorAll('.box').forEach(e => e.remove());
+}
+
+function createSketch(numOfBoxes) {
+
+    function createMultipleBoxes(numOfBoxes) {
+
+        function createBox (numOfBoxes) {
+            const box = document.createElement('div');
+            box.className = "box";
+            box.setAttribute('id','box' + (i + 1));
+                return box;
+        }
+
+        const box = document.getElementById('#container');
+            myBoxes = [],
+            i = 0;
+            for (i; i < (numOfBoxes*numOfBoxes); i++) {
+                myBoxes.push(createBox(numOfBoxes));
+                container.appendChild(myBoxes[i]);
+            }
+    }
+
+    createMultipleBoxes(numOfBoxes);
+
+    function boxEventListener(numOfBoxes) {
+        for (let i = 1; i <= (numOfBoxes*numOfBoxes); i++) {
+            let box = document.getElementById('box' + i);
+            box.addEventListener('mousemove', () => {
+                box.style.backgroundColor = `black`;
+            })
+        }
+    };
+
+    boxEventListener(numOfBoxes);
+
+    function adjustBoxSize(numOfBoxes) {
+        const containerSize = 720;
+        const boxSize = containerSize / numOfBoxes;
+
+        const boxes = document.getElementsByClassName('box');
+
+        for (let i = 0; i < boxes.length; i++) {
+            boxes[i].style.width = `${boxSize}px`;
+            boxes[i].style.height = `${boxSize}px`;
+        }
+    }
+
+    adjustBoxSize(numOfBoxes);
+
+}
+
